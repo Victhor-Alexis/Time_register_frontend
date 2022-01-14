0b9a6e53ce7276d7e938b1506508fa3b0fe5ef4e
Victhor-Alexis <victhoralexis123@gmail.com>
2022-01-14 12:28:55 -0300

    Componente FormCard, seu arcabouço, finalizado.

--- src/components/FormCard/styles.js
+++ src/components/FormCard/styles.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.form`
     width: 100%;
     height: 100%;
-    position: absolute;
+    position: fixed;
     top: 0;
     background-color: blue;
     display: ${ props => props.display === "block" ? "block" : "none" };
@@ -18,6 +18,69 @@ export const Container = styled.div`
 
         .close, .exclude, .confirm {
             cursor: pointer;
+        }
+    }
+
+    .subjectEdit {
+        width: 100%;
+        margin-top: 1.5rem;
+
+        input {
+            width: 35%;
+            display: block;
+            padding: 1rem;
+            margin: 1rem auto;
+            background: none;
+            font-size: 24px;
+            border: 2px solid #fff;
+            color: white;
+
+            ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+                color: #fff;
+                opacity: 0.7;
+            }
+
+            :focus {
+                outline: 2px solid #999;
+            }
+        }
+    }
+
+    .wTimes {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        color: white;
+        margin: 3rem auto 0 auto;
+
+        h1 {
+            width: 100%;
+            text-align: center;
+            margin-bottom: 1rem;
+        }
+
+        .wTime {
+            display: flex;
+            align-items: center;
+            margin: 1rem 2rem;
+
+            input {
+                background: none;
+                border: 2px solid #fff;
+                color: white;
+                width: 4rem;
+                padding: 0.5rem;
+                margin: 0 0.6rem;
+
+                ::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+                    color: #fff;
+                    opacity: 0.8;
+                }
+                
+                :focus {
+                    outline: 2px solid #999;
+                }
+            }
         }
     }
 `;
